552727f3a28bcf69104541372ba769620e7f9bb8
MicaelaEpifanio <micaela.epifanio@gmail.com>
2021-09-09 00:58:24 -0300

    Agregamos un nuevo componente

---
+++ src/components/ItemCount.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Container, Button, Row, Col} from "react-bootstrap";
+const ItemCount = (props) => {
+
+    const [counter, setCounter] = useState(props.initial)
+
+    const increment = () =>  {
+        if(counter < props.stock){
+            setCounter(counter + 1)
+        }
+    }
+
+    const decrement = () => {
+        if(counter > 0){
+            setCounter(counter - 1)
+        }
+    }
+    
+    const onAdd = () => {
+        alert("se agregaron " + counter + " items al carrito") 
+    }
+
+    return (
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col> 
+                        <Button variant="outline-success" onClick={decrement}>-</Button>
+                    </Col>
+                    <Col> 
+                        <p>{counter}</p>
+                    </Col>
+                    <Col> 
+                        <Button variant="outline-success" onClick={increment}>+</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>                 
+                        <Button variant="outline-success" onClick={onAdd}>Agregar al carrito</Button>
+                    </Col>
+                </Row>
+            </Container>
+    )
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,10 @@
 import { Container } from "react-bootstrap";
+import ItemCount from "./ItemCount";
 const ItemListContainer = (props) => {
     return (
             <Container>
-                <p> {props.greeting} </p>   
+                <p> {props.greeting} </p>
+                <ItemCount stock="10" initial="1" />
             </Container>
     )
 }
